b78aeb44b47f5848357be37b0206b451f5ceb499
jtdemey <johntdemey@live.com>
2018-09-14 16:12:54 -0400

    Add files via upload

--- src/routes/expressRoutes.js
+++ src/routes/expressRoutes.js
@@ -1,1 +1,10 @@
+import express from 'express';
+import path from 'path';
 
+const router = express.Router();
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/html/home.html'));
+});
+
+export default router;
